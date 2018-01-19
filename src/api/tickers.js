2a9a4d1c14b0d23f1c46823f62da144d9e151787
Yago Pérez Vázquez <yagoperez@outlook.com>
2018-01-19 16:17:04 +0100

    Add ticker call

--- src/api/tickers.js
+++ src/api/tickers.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
-const getTickers = async cb => {
+const getTickers = async () => {
   const response = await axios.get('https://api.coinmarketcap.com/v1/ticker/?limit=0');
   return response;
 };
 
-export { getTickers };
+const getTickerByID = async (id) => {
+  const response = await axios.get(`https://min-api.cryptocompare.com/data/histoday/?fsym=${id}&tsym=USD&limit=30&e=CCCAGG`);
+  return response;
+};
+
+export { getTickers, getTickerByID };
